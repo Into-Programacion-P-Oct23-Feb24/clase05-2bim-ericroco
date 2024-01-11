4f9bf9a21d96414b944258c5ece5c29ea459f82e
ericroco <erickrodas559@gmail.com>
2024-01-11 15:51:42 -0500

    cambios

---
+++ EjemplosRecursividad/src/paquete03/Clase01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+/**
+ *
+ * @author StarMedia
+ */
+public class Clase01 {
+
+    public static void main(String[] args) {
+    }
+
+    public static String[][] obtenerCadena() {
+        String[][] arreglo = {{"Loja", "Cuenca", "Quito"},
+                {"Zamora","Guayaquil","Huaquillas"}};
+        return arreglo;
+    }
+}
